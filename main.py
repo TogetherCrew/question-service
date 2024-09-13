f563fdf337080cea49b001824b6fbd8406b93562
Cyrille Derche <c.derche@me.com>
2024-09-13 18:55:10 +0200

    add flake8 and isort

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from transformers import pipeline
 from pydantic import BaseModel
 from fastapi import FastAPI
 
-app = FastAPI()
-pipe = pipeline("text-classification", model="shahrukhx01/question-vs-statement-classifier")
 
-custom_labels = {
-    "LABEL_0": "STATEMENT",
-    "LABEL_1": "QUESTION"
-}
+app = FastAPI()
+model = "shahrukhx01/question-vs-statement-classifier"
+pipe = pipeline("text-classification", model=model)
+
+
+custom_labels = {"LABEL_0": "STATEMENT", "LABEL_1": "QUESTION"}
+
 
 class Payload(BaseModel):
-  text: str
+    text: str
+
 
 @app.post("/test")
 async def test(payload: Payload):
-  result = pipe(payload.text)[0]
-  
-  # Customize the label
-  result['label'] = custom_labels.get(result['label'], result['label'])
+    result = pipe(payload.text)[0]
 
-  return result
+    # Customize the label
+    result["label"] = custom_labels.get(result["label"], result["label"])
+    return result
--- test_main.py
+++ test_main.py
@@ -3,8 +3,8 @@ from main import app
 
 client = TestClient(app)
 
-def test_route():
-  response = client.post('/test', json={ "text": "Is this a test?" })
-  assert response.status_code == 200
-  assert response.json()['label'] == 'QUESTION'
 
+def test_route():
+    response = client.post("/test", json={"text": "Is this a test?"})
+    assert response.status_code == 200
+    assert response.json()["label"] == "QUESTION"
