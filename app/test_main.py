c86a12c4223c6fd8de913647b0d9948f5c726f87
Cyrille Derche <c.derche@me.com>
2024-09-13 20:27:44 +0200

    fix tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,10 @@
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app)
-
 
 def test_route():
-    response = client.post("/test", json={"text": "Is this a test?"})
-    assert response.status_code == 200
-    assert response.json()["label"] == "QUESTION"
+
+    with TestClient(app) as client:
+        response = client.post("/test", json={"text": "Is this a test?"})
+        assert response.status_code == 200
+        assert response.json()["label"] == "QUESTION"
